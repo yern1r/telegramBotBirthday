4a58e96bc8d92c89e5de4a2e8e3bfc54d6220b5a
yern1r <yernaryelemess@gmail.com>
2023-12-20 15:16:47 +0600

    first commit

---
+++ src/main/java/com/yern1r/birthdaybot/BirthDayBotApplication.java
@@ -0,0 +1,13 @@
+package com.yern1r.birthdaybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthDayBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthDayBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yern1r/birthdaybot/BirthDayBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yern1r.birthdaybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthDayBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
